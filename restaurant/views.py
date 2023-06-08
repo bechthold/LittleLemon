05a6e64dbf6bcf121c672e92811df148162df49a
Igor Bechthold <igor.bechthold@gmail.com>
2023-06-08 10:17:55 +0200

    add authentication

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,24 +1,35 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from requests import Response
 from rest_framework.viewsets import ModelViewSet
-
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
-
-from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 from .models import Booking, Menu
-
 from rest_framework import viewsets, permissions
 from django.contrib.auth.models import User
+
+from rest_framework.permissions import IsAuthenticated
+
+
+# from rest_framework.authtoken.models import Token
+# from rest_framework.decorators import api_view, permission_classes
+#
+# token = Token.objects.create(user=...)
+# print(token.key)
+#
+#
+# @api_view
+# @permission_classes([IsAuthenticated])
+# def securedView(request):
+#     return Response({"message": "need authentication"})
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class MenuItemsView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -29,5 +40,6 @@ class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
 
 
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
